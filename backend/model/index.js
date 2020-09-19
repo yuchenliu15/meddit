82ef9d51d294fe70e6073e9a9979914a8db431b2
yuchen <yl6448@nyu.edu>
2020-09-18 19:49:56 -0700

    init firebase in model

---
+++ backend/model/index.js
@@ -0,0 +1,14 @@
+const firebase = require('firebase')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIhfL02ma5ynLQh2z8Xvy-5y1UwExwcJc",
+    authDomain: "meddit-25e2b.firebaseapp.com",
+    databaseURL: "https://meddit-25e2b.firebaseio.com",
+    projectId: "meddit-25e2b",
+    storageBucket: "meddit-25e2b.appspot.com",
+    messagingSenderId: "917960780816",
+    appId: "1:917960780816:web:e3a2fee2c1b67e987dae95",
+    measurementId: "G-E3M53LWKT1"
+};
+
+export default firebase.initializeApp(firebaseConfig)
