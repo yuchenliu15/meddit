390d05ce123933c8b6d3105893e363cce0dc0049
yuchen <yl6448@nyu.edu>
2020-09-19 14:51:47 -0700

    add post request to create user

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,13 +1,22 @@
 import React, {useState } from 'react';
 import { OutlinedInput, Button } from '@material-ui/core';
+import axios from 'axios'
 export default () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const onUsernameChange = (e) => { setUsername(e.target.value)}
-    const onPasswordChange = (e) => { setUsername(e.target.value)}
+    const onPasswordChange = (e) => { setPassword(e.target.value)}
     const onClick = () => {
-        console.log(username)
+        axios.post('http://localhost:3000/users/create',
+            {
+                username,
+                password
+            }).then((data) => {
+                console.log(data)
+            }).catch(e => {
+                console.log(e)
+            })
     }
     return (
         <div>
