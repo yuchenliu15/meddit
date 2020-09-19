177834624cb3182c1145364fd645b8b81016cf2c
nysteo <teonys@nyu.edu>
2020-09-19 15:11:38 -0400

    Set up medical Component

---
+++ frontend/src/pages/Medical.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import SymptomLog from '../components/medical/SymptomLog';
+import {Grid,Typography, makeStyles, Box, Divider} from '@material-ui/core';
+
+import Image from '../assets/background.svg';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+const Medical = (props) => {
+    return (
+        <Typography> This is the medical Component</Typography>
+    )
+}
+
+export default Medical;
