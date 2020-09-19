ee12ab4896b486e1dd6123176a46252b495f8fe1
yuchen <yl6448@nyu.edu>
2020-09-19 14:01:01 -0700

    add constants and router package

---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,7 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const COMMUNITY = '/community'
+export const POST = '/post'
