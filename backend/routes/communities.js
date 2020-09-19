4bf69962793ce6dd74c042f9d0915182e3004061
yuchen <yl6448@nyu.edu>
2020-09-19 00:36:56 -0700

    refactor communities

--- backend/routes/communities.js
+++ backend/routes/communities.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const Communities = require('../model/Communities')
-var CommunitiesTable = new Communities();
+const community = new Communities();
 
 
 // The community page for this community
 router.get('/:id', function(req, res, next) {
   const id = req.params.id;
-  res.send(CommunitiesTable.get(id));
+  community.get(id).then((data) => {
+    res.send(data);
+  })
 });
 
 // All communities for this user
 router.get('/', function(req, res, next) {
-  res.send(CommunitiesTable.getAll());
+  community.getAll().then((data) => {
+    res.send(data);
+  })
 });
 
+router.post('/', function(req, res, next) {
+  const name = req.body.name
 
+  community.create(name)
+    .then(() => res.status(200).end())
+});
 
 module.exports = router;
