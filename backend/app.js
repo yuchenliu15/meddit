1a1d3d8c6000a7969cbb2253a98000fa2388f7a7
yuchen <yl6448@nyu.edu>
2020-09-18 21:17:36 -0700

    add auth middleware

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { authenticateToken } = require('./middleware/auth')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-
+app.use(authenticateToken);
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
