6fcf6a6f3673c4b5c5521b0d474a19ddde2cfe74
nysteo <teonys@nyu.edu>
2020-09-19 15:24:11 -0400

    adding SymptomLog

---
+++ frontend/src/components/medical/SymptomLog.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import {Button, Card, CircularProgress, Grid, CardContent, makeStyles, Divider, Typography, Checkbox} from '@material-ui/core';
+import InputBase from '@mater-ui/core/InputBase';
+
+const useStyles = makeStyles((theme) => ({
+
+}))
+
+const SymptomLog = (props) => {
+    return (
+        <Typography>This is the Symptom Log</Typography> 
+    )
+}
+
+export default SymptomLog;
